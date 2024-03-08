ea87e92fb825e729f9fbddcb6c00ae1de57b9bfa
Chin Chin <ssscnri@gmail.com>
2024-03-09 00:35:09 +0800

    project setup

---
+++ app/src/main/java/com/sssunri/sixstore/SixstoreApplication.java
@@ -0,0 +1,13 @@
+package com.sssunri.sixstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixstoreApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/sssunri/sixstore/SixstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sssunri.sixstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
