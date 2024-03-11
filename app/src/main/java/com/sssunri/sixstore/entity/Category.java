a56e927616d7708a688566ba499effcf8e93fc17
Chin Chin <ssscnri@gmail.com>
2024-03-11 12:25:34 +0800

    add jpa annotations for category entity

---
+++ app/src/main/java/com/sssunri/sixstore/entity/Category.java
@@ -0,0 +1,45 @@
+package com.sssunri.sixstore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private byte categoryId;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    // constructors, getters and setters
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public byte getCategoryId() {
+        return categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // toString() method
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
