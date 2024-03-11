3e578fcba3cc6d9814a32c656e21e7ee807ce18a
Chin Chin <ssscnri@gmail.com>
2024-03-11 22:33:12 +0800

    refactor code to create or retrieve category

--- app/src/main/java/com/sssunri/sixstore/service/CategoryService.java
+++ app/src/main/java/com/sssunri/sixstore/service/CategoryService.java
@@ -4,18 +4,30 @@ import com.sssunri.sixstore.dao.CategoryDAO;
 import com.sssunri.sixstore.entity.Category;
 
 public class CategoryService {
-    public static void createCategory(CategoryDAO categoryDAO, String name) {
+    public static Category createOrRetrieveCategory(CategoryDAO categoryDAO, String name) {
+        // try to retrieve the category by name
+        Category category = categoryDAO.findByName(name);
+
+        // if the category does not exist, create a new one
+        if (category == null) {
+            createAndSaveCategory(categoryDAO, name);
+        }
+
+        return category;
+    }
+
+    private static void createAndSaveCategory(CategoryDAO categoryDAO, String name) {
         try {
             // creating a new category object
             System.out.println("creating a new category object...");
-            Category tempCategory = new Category(name);
+            Category category = new Category(name);
 
             // saving the category object
             System.out.println("saving the category...");
-            categoryDAO.save(tempCategory);
+            categoryDAO.save(category);
 
             // displaying the id of the saved category
-            System.out.println("saved category. generated id: " + tempCategory.getCategoryId());
+            System.out.println("saved category. generated id: " + category.getCategoryId());
         } catch (Exception e) {
             // handle exceptions appropriately (log or rethrow)
             System.err.println("error while creating and saving the category: " + e.getMessage());
