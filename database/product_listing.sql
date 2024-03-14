388d4ab0d48c198a383164d397cfefe9a3e6a64f
Chin Chin <ssscnri@gmail.com>
2024-03-14 19:27:26 +0800

    update product listing table

--- database/product_listing.sql
+++ database/product_listing.sql
@@ -1,22 +1,24 @@
-CREATE DATABASE IF NOT EXISTS store;
-USE store;
+CREATE DATABASE IF NOT EXISTS sixstore;
+USE sixstore;
 
-CREATE TABLE category (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE product_listing (
-    product_id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS product_listing (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sku VARCHAR(15) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    price DECIMAL(5 , 2 ) NOT NULL,
-    quantity INT NOT NULL,
+    unit_price DECIMAL(5, 2) NOT NULL,
+    units_in_stock INT NOT NULL,
+    active BOOLEAN DEFAULT TRUE,
     category_id INT,
     image_url VARCHAR(255),
     rating INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (category_id)
-        REFERENCES category (category_id)
+        REFERENCES category (id)
 );
