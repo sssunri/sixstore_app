b468281d4b187bc357a073f5c32b3b3ee74b5e4e
Chin Chin <ssscnri@gmail.com>
2024-03-19 16:20:00 +0800

    add configuration to disable certain http methods

---
+++ app/src/main/java/com/sssunri/sixstore/config/DataRestConfig.java
@@ -0,0 +1,31 @@
+package com.sssunri.sixstore.config;
+
+import com.sssunri.sixstore.entity.Category;
+import com.sssunri.sixstore.entity.ProductListing;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable http methods for product listings
+        config.getExposureConfiguration()
+                .forDomainType(ProductListing.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // disable http methods for product category
+        config.getExposureConfiguration()
+                .forDomainType(Category.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
