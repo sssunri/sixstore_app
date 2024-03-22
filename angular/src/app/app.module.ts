eb6a83b7ac1be53bff69a64ba3cd6e894d65d04b
Chin Chin <ssscnri@gmail.com>
2024-03-22 18:08:02 +0800

    move navigation menu component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
 
 @NgModule({
   declarations: [
