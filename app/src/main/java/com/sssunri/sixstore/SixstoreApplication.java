153f253493d53a5035d48929982976d59ecffa09
Chin Chin <ssscnri@gmail.com>
2024-03-22 21:54:30 +0800

    exclude security auto-configuration

--- app/src/main/java/com/sssunri/sixstore/SixstoreApplication.java
+++ app/src/main/java/com/sssunri/sixstore/SixstoreApplication.java
@@ -10,7 +10,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
+)
 public class SixstoreApplication {
 
 	public static void main(String[] args) {
