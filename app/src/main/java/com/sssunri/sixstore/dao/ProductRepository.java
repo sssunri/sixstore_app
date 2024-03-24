a9bd6016402456ad3efec140a2d0392d16d16509
Chin Chin <ssscnri@gmail.com>
2024-03-24 17:54:47 +0800

    add cross-origin annotation

--- app/src/main/java/com/sssunri/sixstore/dao/ProductRepository.java
+++ app/src/main/java/com/sssunri/sixstore/dao/ProductRepository.java
@@ -3,7 +3,9 @@ package com.sssunri.sixstore.dao;
 import com.sssunri.sixstore.entity.ProductListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "products", path = "product-listings")
 public interface ProductRepository extends JpaRepository<ProductListing, Short> {
 }
